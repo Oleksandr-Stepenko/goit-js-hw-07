f3a1d324c350e3180ec8cbecd3a96590377fcd74
Oleksandr-Stepenko <113541212+Oleksandr-Stepenko@users.noreply.github.com>
2023-02-21 18:51:29 +0200

    add 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryConteiner = document.querySelector(".gallery");
+const picturesMarkup = createPicturesCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryConteiner.insertAdjacentHTML("beforeend", picturesMarkup);
+
+galleryConteiner.addEventListener("click", onGalleryConteinerClick);
+
+function createPicturesCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				data-source="${original}"
+				alt="${description}"
+			/>
+		</a>
+	</div>`;
+    })
+    .join("");
+}
+
+function onGalleryConteinerClick(e) {
+  // console.log(e.target.nodeName);
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}">
+`);
+  instance.show();
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
